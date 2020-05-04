827b436a2f2f59542fc0d2aaf33dbf4d71d0f93d
Sergio Perez <perez.sergio.ace@gmail.com>
2020-05-03 23:58:59 -0400

    finished adding the animations for the page

---
+++ app.js
@@ -0,0 +1,17 @@
+const t1 = new TimelineMax({ paused: true })
+
+t1.to('.menu-container', 0.4, {
+	right: '0%'
+})
+
+t1.staggerFrom('.link', 0.4, { opacity: 0 }, 0.1, '-=0.2')
+
+t1.reverse()
+
+$(document).on('click', '.open-menu', function() {
+	t1.reversed(!t1.reversed())
+})
+
+$(document).on('click', '.close-menu', function() {
+	t1.reversed(!t1.reversed())
+})
